ee48aa8c7bbc4846cdc9bf1fb6f8f1557900d71e
brerofons <bfonsecarodri@gmail.com>
2022-10-18 11:28:35 -0300

    Criação do link do whatsapp
    Foi criado um link com mensagem para o WhatsApp para finalizar a compra.

---
+++ linkzap.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include (' database_blabla.php ');
+    $num = "5531973179750";
+    $nmP = $ sql = "SELECT * FROM MyTable ";
+    $cor $ sql = "SELECT * FROM MyTable ";
+    $tama $ sql = "SELECT * FROM MyTable ";
+    $prod = "$nmP da cor: $cor, no tamanho: $tama.
+    ----------------------";
+    if(){
+    <a href="https://wa.me/$num?text=Olá gostaria de comprar os seguinte produtos:
+    ----------------------
+    $prod
+    Gostaria de saber o preço?"></a>
+    }else{
+    <a href="https://wa.me/$num?text=Olá gostaria de comprar os seguinte produtos:
+    ----------------------
+    $prod
+    Gostaria de saber o preço?"></a>
+    };
+    ?>
+</body>
+</html>
