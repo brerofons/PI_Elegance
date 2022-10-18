e61b9b57a3571b8b33d33046637d0367a1860787
Gabriel Martins <108273492+gabrieledumartins@users.noreply.github.com>
2022-10-18 11:32:50 -0300

    tela de escolha v 1.2
    tela de escolha quase feita

--- Escolha/carrinho.php
+++ Escolha/carrinho.php
@@ -69,6 +69,9 @@
                     </div>
                 </div>
             </div>
+            <?php
+            
+            ?>
             <div class="pad"></div>
             <div class="caixa">
                 <div class="row">
