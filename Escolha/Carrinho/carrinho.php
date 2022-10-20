606c27ebd3ce55183123f72590f27c0602447edd
Gabriel Martins <108273492+gabrieledumartins@users.noreply.github.com>
2022-10-20 11:35:00 -0300

    carrinho de compras v1.0

--- Escolha/Carrinho/carrinho.php
+++ Escolha/Carrinho/carrinho.php
@@ -8,24 +8,25 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
         <!-- -=-=-| LINKS |-=-=- -->
-        <link rel="stylesheet" href="EscolhaP.css">
+        <link rel="stylesheet" href="Carrinho.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     
         <title>Carrinho</title>
     </head>
     <body class="cor">
-        <!--Cabeçalho Inicio-->
-    <header>
-        <div class="container-fluid cabecalho">
-            <div class="row cont">
-                <div class="col d-flex justify-content-between">
-                    <a href="../Index/Index.html"> <img src="../imagens/img-cabecalho/Logo.png" class="logo" alt="Logo da Empresa"></a>
-                    <h4 style="color: white ; padding-top: 3vh;" >Compras Feitas Pelo Whatsapp</h4> 
-                    <a href="carrinho.html"> <img src="../imagens/img-cabecalho/carrimho.png" class="logo" alt="Carrinho de Compras"></a>
+    <body class="body pagina cor">
+        <!-- -=-=-| Cabeçalho Inicio |-=-=- -->
+        <header>
+            <div class="container-fluid cabecalho">
+                <div class="row cont">
+                    <div class="col d-flex justify-content-between">
+                        <a href="../Index/Index.html"> <img src="../../imagens/img-cabecalho/logo"  class="logo" alt="Logo da Empresa"></a>
+                        <h4 class="texto" >Compras Feitas Pelo Whatsapp</h4> 
+                        <a href="../Carrinho/carrinho.php"> <img src="../../imagens/img-cabecalho/carrimho.png"  style="padding: 10px ;" class="carrinho" alt="Carrinho de Compras"></a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
+        </header>
     <!--Cabeçalho Fim-->
     <!--Produtos inicio-->
     <main class="conteudo">
@@ -55,23 +56,23 @@
                     <div class="col-5">
                         <h5>Produto generico de cor e tamanho aleatorio</h5>
                     </div>
-                    <div class="col-1">
-                        <button class="d-flex justify-content-end">+</button>
-                    </div>
-                    <div class="col-1">
-                        <p class="d-flex justify-content-start">1</p>
-                    </div>
-                    <div class="col-1">
-                        <button class="d-flex justify-content-end">-</button>
-                    </div>
+                    <div class="quantidade">
+                        <button type="button" onclick="this.parentNode.querySelector('[type=number]').stepDown();" class="bot">
+                            <p>-</p>
+                        </button>
+                        
+                        <input type="number" name="number" min="1" max="10" value="1">
+                        <!--readonly-->
+                        
+                        <button type="button" onclick="this.parentNode.querySelector('[type=number]').stepUp();"class="bot">
+                            <p>+</p>
+                        </button>
+                        </div>
                     <div class="col-1">
                         <img src="" alt="">
                     </div>
                 </div>
             </div>
-            <?php
-            
-            ?>
             <div class="pad"></div>
             <div class="caixa">
                 <div class="row">
@@ -100,10 +101,10 @@
                 <div class="col-4">
                     <div class="row ">
                         <div class="col-6 d-flex justify-content-center">
-                            <a href="https://www.instagram.com/"><img class="icon" src="../imagens/img-rodape/instagram.png" alt=""></a>
+                            <a href="https://www.instagram.com/"><img class="icon" src="../../imagens/img-rodape/instagram.png" alt=""></a>
                         </div>
                         <div class="col-6 d-flex justify-content-center">
-                            <a href="https://www.whatsapp.com/?lang=pt_br"><img class="icon" src="../imagens/img-rodape/whatsapp.png" alt=""></a>
+                            <a href="https://www.whatsapp.com/?lang=pt_br"><img class="icon" src="../../imagens/img-rodape/whatsapp.png" alt=""></a>
                         </div>
                     </div>
                 </div>
@@ -115,7 +116,7 @@
                     </div>
                 </div>
         </div>
-        </footer>
+    </footer>
     <!-- -=-=-| Footer Fim |-=-=- -->
     </body>
 </html>
