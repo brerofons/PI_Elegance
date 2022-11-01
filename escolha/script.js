284e8f7db74379175f31a8d4b1917a9b3112ac71
Gabriel Martins <108273492+gabrieledumartins@users.noreply.github.com>
2022-11-01 11:21:04 -0300

    OI

--- escolha/script.js
+++ escolha/script.js
@@ -1,8 +1,6 @@
-
-
-
-var corselecionada
 var tamanhoselecionado
+var corselecionada
+var metodopagamento
 
 function selectedsize(y) {
 
@@ -15,18 +13,23 @@ function collorselect(x) {
 
 }
 
+function paymentselected(z) {
+
+    metodopagamento = z.querySelector("h5").innerHTML;
+
+}
+
 function gotowhatsapp() {
 
     var quantidade = document.getElementById("quantidade").value;
     var nome = document.getElementById("nome").value;
-    var pagamento = document.getElementById("pagamento").value;
 
     var url = "https://wa.me/55975785997?text="
         + "Nome: " + nome + "%0a"
         + "Cor: " + corselecionada + "%0a"
         + "Tamanho: " + tamanhoselecionado + "%0a"
         + "Quantidade: " + quantidade + "%0a"
-        + "Metodo de pagamento: " + pagamento;
+        + "Metodo de pagamento: " + metodopagamento;
 
     window.open(url, '_blank').focus();
 }
