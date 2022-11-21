723793d9ec33f7aaab95ac24bef5e4584d473a99
Gabriel Martins <108273492+gabrieledumartins@users.noreply.github.com>
2022-11-21 10:26:06 -0300

    OI

--- index1/home.php
+++ index1/home.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -11,12 +10,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="Cadastro/cadastro.css">
-    <link rel="stylesheet" href="Cadastro/cadastro.js">
-    <script src="https://kit.fontawesome.com/c65e50066a.js" crossorigin="anonymous"></script>
-
+    <link rel="stylesheet" href="../Cadastro/cadastro.css">
+    <link rel="stylesheet" href="../Cadastro/cadastro.js">
     <title>Cadastro de produto</title>
 </head>
+<body>
 <?php
   
   if ( !isset ( $_SESSION ) ) {
@@ -33,7 +31,6 @@
   }
 
 ?>
-<body>
     <header>
         <div class="container-fluid text-center pt-1 pb-1">
            <h2>Ellegance Calçados</h2> 
