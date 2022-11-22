8c201331d26962a20ceb780f8f34b1ec426464a6
brerofons <bfonsecarodri@gmail.com>
2022-11-22 10:27:40 -0300

    Mudança imagens
    A fonte foi mudada e tambem coloquei as imagens certas em alguns produtos.

--- escolha/escolha.php
+++ escolha/escolha.php
@@ -2,6 +2,7 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="description" content="Elegance">
         <meta name="keywords" content="Sapatos, Anabela, Tamanco,">
         <meta name="author" content="Gabriel Martins, Breno Rodrigues">
